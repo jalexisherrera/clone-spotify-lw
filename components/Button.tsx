5c10f95e750b49f8ac56dae306873e77cbeaa6aa
Jaime Alexis Herrera R <jalexis.herrera@udea.edu.co>
2023-09-19 10:32:08 -0500

    add more components

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,17 @@
 interface ButtonProps {
   text: string;
+  type?: 'primary' | 'secondary';
+  handleClick?: () => void;
 }
 
-const Button = ({ text }: ButtonProps) => {
+const Button = ({ text, type = 'primary', handleClick = () => {} }: ButtonProps) => {
   return (
-    <button className='bg-white text-black rounded-full px-3 py-1 font-semibold hover:scale-105'>
+    <button
+      onClick={handleClick}
+      className={`bg-white text-black rounded-full ${
+        type === 'primary' ? 'px-8 py-3 font-semibold' : 'px-3 py-1'
+      } font-semibold hover:scale-105`}
+    >
       {text}
     </button>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,19 @@
+import { Navbar } from '@/components/Navbar';
+import { PlaylistContainer } from '@/components/PlaylistContainer';
 import { Sidebar } from '@/components/Sidebar';
 
 const Home = () => {
   return (
     <div className='bg-black text-white w-full h-screen flex'>
       <Sidebar />
-      <main className='debug w-full'>
-        <nav>
-          <div>flechas de navegacion</div>
-          <div>
-            <span>premium support</span>
-            <span>download</span>
-            <span>sign up</span>
-            <span>log in</span>
-          </div>
-        </nav>
-        <div>
-          <section>spotify playlists</section>
-          <section>focus</section>
+      <main className='w-full p-2 h-full'>
+        <div className='container-card flex flex-col h-full'>
+          <Navbar />
+          <div className='bg-gradient-to-t from-spotify-light-black to-spotify-light-black-3 px-4 h-full flex flex-col gap-7'>
+            <PlaylistContainer name={'Spotify Playlist'} />  
+            <PlaylistContainer name={'Focus'} />
+          </div>    
+          
         </div>
       </main>
     </div>
