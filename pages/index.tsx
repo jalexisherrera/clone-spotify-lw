74bd693c4a51925bc3476a9fefbe50c4d2f968a1
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2023-09-20 07:38:13 -0500

    agregar fetching de datos con swr al api de datos de playlists

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import { Navbar } from '@/components/Navbar';
 import { PlaylistContainer } from '@/components/PlaylistContainer';
 import { Sidebar } from '@/components/Sidebar';
-import { playlists } from '@/datos';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Home = () => {
+  const { data, isLoading } = useSWR('/api/data', fetcher);
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className='bg-black text-white w-full h-screen flex'>
       <Sidebar />
@@ -11,8 +17,8 @@ const Home = () => {
         <div className='container-card flex flex-col h-full'>
           <Navbar />
           <div className='bg-gradient-to-t from-spotify-light-black to-spotify-light-black-3 px-4 h-full flex flex-col gap-7'>
-            <PlaylistContainer data={playlists.playlists} name={'Spotify Playlists'} />
-            <PlaylistContainer data={playlists.focus} name={'Focus'} />
+            <PlaylistContainer data={data.playlists} name={'Spotify Playlists'} />
+            <PlaylistContainer data={data.focus} name={'Focus'} />
           </div>
         </div>
       </main>
